97df41a70e51075f157455182554f0165fe9fe38
JPromi <jonasmpr05@gmail.com>
2025-03-03 10:12:21 +0100

    init

---
+++ src/test/java/com/jpromi/avion/darts/DartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpromi.avion.darts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
