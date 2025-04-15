1984d279b21c7df416988626c1fd5188b203b05e
JPromi <jonasmpr05@gmail.com>
2025-04-15 07:43:27 +0200

    add web config

---
+++ src/main/java/com/jpromi/darts/backend/configuration/WebConfiguration.java
@@ -0,0 +1,15 @@
+package com.jpromi.darts.backend.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("https://dev.jpromi.com")
+                .allowedHeaders("*").allowCredentials(true);
+    }
+}
