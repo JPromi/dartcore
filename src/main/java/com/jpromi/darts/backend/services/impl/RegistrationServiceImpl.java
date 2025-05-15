42d47bb945830ef7693f28be8c84a4ff02c6aab4
JPromi <jonasmpr05@gmail.com>
2025-05-15 10:21:34 +0200

    add send registration mail

--- src/main/java/com/jpromi/darts/backend/services/impl/RegistrationServiceImpl.java
+++ src/main/java/com/jpromi/darts/backend/services/impl/RegistrationServiceImpl.java
@@ -2,22 +2,35 @@ package com.jpromi.darts.backend.services.impl;
 
 import com.jpromi.darts.backend.entities.Account;
 import com.jpromi.darts.backend.entities.Profile;
+import com.jpromi.darts.backend.models.EmailObject;
 import com.jpromi.darts.backend.models.RegisterRequest;
 import com.jpromi.darts.backend.repositories.AccountRepository;
+import com.jpromi.darts.backend.services.MailService;
 import com.jpromi.darts.backend.services.RegistrationService;
+import com.jpromi.darts.backend.services.TemplateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import com.password4j.Password;
 
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class RegistrationServiceImpl implements RegistrationService {
 
     @Autowired
     private AccountRepository accountRepository;
+
+    @Autowired
+    private MailService mailService;
+
+    @Autowired
+    private TemplateService templateService;
+
+    @Value("${com.jpromi.darts.app.domain}")
+    private String appDomain;
 
     @Override
     public Account register(RegisterRequest registerRequest) {
@@ -36,6 +49,34 @@ public class RegistrationServiceImpl implements RegistrationService {
                 .build();
 
         accountRepository.save(account);
+
+        EmailObject mailObject = EmailObject.builder()
+                .to(new ArrayList<>(List.of(registerRequest.getEmail())))
+                .subject("Email Verification")
+                .build();
+
+        HashMap<String, String> templateVariables = new HashMap<>(
+            Map.of(
+            "confirmationLink", appDomain + "/barrier/register/" + account.getEmailVerificationToken(),
+            "name", registerRequest.getUsername()
+            )
+        );
+
+        mailObject.setHtmlBody(
+            templateService.generateTemplateFromFile(
+                "src/main/resources/templates/mail/registration.html",
+                templateVariables
+            )
+        );
+
+        mailObject.setBody(
+            templateService.generatePlainTextFromFile(
+                "src/main/resources/templates/mail/registration.html",
+                templateVariables
+            )
+        );
+
+        mailService.send(mailObject);
 
         return account;
     }
