0e6d58a4e5ceea23951e02f264d2252b88a637a6
Jonas <jonasmpr05@gmail.com>
2025-11-03 11:16:24 +0100

    use domain from application.properties

--- src/main/java/com/jpromi/darts/backend/configuration/WebConfiguration.java
+++ src/main/java/com/jpromi/darts/backend/configuration/WebConfiguration.java
@@ -1,5 +1,6 @@
 package com.jpromi.darts.backend.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,9 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
+    @Value("${com.jpromi.darts.app.domain}")
+    private String appDomain;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*").allowedOrigins("https://dev.jpromi.com")
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins(appDomain)
                 .allowedHeaders("*").allowCredentials(true);
     }
 }
