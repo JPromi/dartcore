8d013cf9d290f94150d8168e89897c324925d520
JPromi <jonasmpr05@gmail.com>
2026-05-04 09:31:26 +0200

    fix frontend serve

--- backend/src/main/java/com/jpromi/darts/backend/controllers/FrontendController.java
+++ backend/src/main/java/com/jpromi/darts/backend/controllers/FrontendController.java
@@ -1,4 +1,19 @@
 package com.jpromi.darts.backend.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class FrontendController {
+
+    @GetMapping(value = {
+            "/",
+            "/{path:[^\\.]*}",
+            "/{a:[^\\.]+}/{path:[^\\.]*}",
+            "/{a:[^\\.]+}/{b:[^\\.]+}/{path:[^\\.]*}",
+            "/{a:[^\\.]+}/{b:[^\\.]+}/{c:[^\\.]+}/{path:[^\\.]*}"
+    })
+    public String handleForward() {
+        return "forward:/index.html";
+    }
 }
